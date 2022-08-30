00df56c07c438b3de09ab5a240401842e52841cd
BorisKiselev <hugh.dancy@icloud.com>
2022-08-30 18:01:18 +0700

    Initial Commit

---
+++ Homework17/Homework17App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework17App.swift
+//  Homework17
+//
+//  Created by Борис Киселев on 30.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Homework17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
